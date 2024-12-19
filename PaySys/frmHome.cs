0389d00dbf5cf2ae07b3fedd8ec85b5c2bd295f4
SeanSaldevia <abarquezandrey0@gmail.com>
2024-12-19 14:37:44 +0800

    Update frmHome.cs

--- PaySys/frmHome.cs
+++ PaySys/frmHome.cs
@@ -13,7 +13,7 @@ namespace Group2PaySys
 {
     public partial class frmHome : Form
     {
-        OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = PayrollSystemDatabase.accdb; Persist Security Info = True;Jet OLEDB:Database Password = jarmjpayrollsystem");
+        OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = PayrollSystemDatabase.accdb; Persist Security Info = True;Jet OLEDB:Database Password = payrollsystem");
         public frmHome()
         {
             InitializeComponent();
