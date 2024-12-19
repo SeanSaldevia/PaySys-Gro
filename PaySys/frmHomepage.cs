e52f5b13f600c4ba34ed0d3960e92160e7723505
SeanSaldevia <abarquezandrey0@gmail.com>
2024-12-19 14:38:02 +0800

    Update frmHomepage.cs

--- PaySys/frmHomepage.cs
+++ PaySys/frmHomepage.cs
@@ -13,7 +13,7 @@ namespace Group2PaySys
 {
     public partial class frmHomepage : Form
     {
-        OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = PayrollSystemDatabase.accdb; Persist Security Info = True;Jet OLEDB:Database Password = jarmjpayrollsystem");
+        OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = PayrollSystemDatabase.accdb; Persist Security Info = True;Jet OLEDB:Database Password = payrollsystem");
         public frmHomepage()
         {
             InitializeComponent();
